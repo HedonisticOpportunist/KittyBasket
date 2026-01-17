39caae135e75fe0269be4edac8b95dbffe5006ca
HedonisticOpportunist <fujitahibaraki@gmail.com>
2026-01-17 03:09:53 +0100

    Format GetMenuItems.cs for consistency

--- CMSGTechnical.Mediator/Menu/GetMenuItems.cs
+++ CMSGTechnical.Mediator/Menu/GetMenuItems.cs
@@ -1,4 +1,4 @@
-﻿using CMSGTechnical.Domain.Interfaces;
+using CMSGTechnical.Domain.Interfaces;
 using CMSGTechnical.Domain.Models;
 using CMSGTechnical.Mediator.Dtos;
 using MediatR;
@@ -10,7 +10,6 @@ namespace CMSGTechnical.Mediator.Menu
 
     public class GetMenuItemsHandler : IRequestHandler<GetMenuItems, IEnumerable<MenuItemDto>>
     {
-
         private IRepo<MenuItem> MenuItems { get; }
 
         public GetMenuItemsHandler(IRepo<MenuItem> menuItems)
@@ -18,14 +17,14 @@ namespace CMSGTechnical.Mediator.Menu
             MenuItems = menuItems;
         }
 
-        public async Task<IEnumerable<MenuItemDto>> Handle(GetMenuItems request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<MenuItemDto>> Handle(
+            GetMenuItems request,
+            CancellationToken cancellationToken
+        )
         {
-            var q = MenuItems.GetAll().OrderBy(m => m.Price); // Sort menu items by price. 
+            var q = MenuItems.GetAll().OrderBy(m => m.Price); // Sort menu items by price.
             var r = await q.ToListAsync(cancellationToken);
             return r.ToDto();
         }
     }
-
-
-
 }
