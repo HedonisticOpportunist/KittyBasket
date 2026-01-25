0dac48e1e3d4ace567c70ba3523493f777fc2597
HedonisticOpportunist <fujitahibaraki@gmail.com>
2026-01-25 20:24:22 +0100

    Update basket.js

--- CMSGTechnical/wwwroot/basket.js
+++ CMSGTechnical/wwwroot/basket.js
@@ -1,4 +1,9 @@
 window.basketStore = {
+  // The localStorage object stores data with no expiration date.
+  // In this case, the items in the basket remain saved even after the browser is closed.
   save: (basket) => localStorage.setItem("basket", JSON.stringify(basket)),
+
+  // The "load" method retrieves the saved basket from localStorage.
+  // If nothing has been saved, it returns an empty object instead.
   load: () => JSON.parse(localStorage.getItem("basket") || "{}"),
 };
