47d65118bfdbacf7ad9d9c3fbe6666f5f367fcac
HedonisticOpportunist <fujitahibaraki@gmail.com>
2026-01-25 20:36:35 +0100

    Update GetMenuItems.cs

--- CMSGTechnical.Mediator/Menu/GetMenuItems.cs
+++ CMSGTechnical.Mediator/Menu/GetMenuItems.cs
@@ -6,14 +6,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CMSGTechnical.Mediator.Menu
 {
+    // Query object representing a request to retrieve all menu items.
     public record GetMenuItems : IRequest<IEnumerable<MenuItemDto>>;
 
     public class GetMenuItemsHandler : IRequestHandler<GetMenuItems, IEnumerable<MenuItemDto>>
     {
+        // Repository abstraction for accessing MenuItem domain models.
         private IRepo<MenuItem> MenuItems { get; }
 
         public GetMenuItemsHandler(IRepo<MenuItem> menuItems)
         {
+            // Inject the menu item repository.
             MenuItems = menuItems;
         }
 
@@ -22,8 +25,13 @@ namespace CMSGTechnical.Mediator.Menu
             CancellationToken cancellationToken
         )
         {
-            var q = MenuItems.GetAll().OrderBy(m => m.Price); // Sort menu items by price.
+            // Query all menu items and sort them by price.
+            var q = MenuItems.GetAll().OrderBy(m => m.Price);
+
+            // Execute the query against the database.
             var r = await q.ToListAsync(cancellationToken);
+
+            // Convert domain models to DTOs for the caller.
             return r.ToDto();
         }
     }
